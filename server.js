e493b95de2c104589550917dc8d1fc9a063c7221
Muhinthan <m.tharmendran@gmail.com>
2016-09-21 03:45:06 +0000

    Fix port

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ app.get('/', function(req, res){
   res.json(headerInfo);
 });
 
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Listening on port ', port);
+});
